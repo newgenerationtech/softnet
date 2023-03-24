8beab4fd5f4a9194b1ecb809f775302c4f5dcd1f
newgenerationtech <128788438+newgenerationtech@users.noreply.github.com>
2023-03-24 05:17:43 -0700

    Add files via upload

---
+++ About.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+  <link rel="stylesheet" href="Css/Styles.css">
+  <link rel="stylesheet" href="Css/bootstrap.min.css">
+  <link rel="stylesheet" href="Css/landing_styles.css">
+  <title>softNet</title>
+  <link rel="shortcut icon" type="image" href="Uploads.icon.JPG">
+</head>
+<body>
+  <!-- NAVBAR -->
+  <div style="height:10px; background:#27aae1;"></div>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <a href="#" class="navbar-brand"> <marquee>SoftNet.com</marquee></a>
+      <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarcollapseCMS">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarcollapseCMS">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a href="index.php?Page=1" class="nav-link"> <i class="fas fa-user text-success"></i> Home</a>
+        </li>
+          <li class="nav-item">
+      
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">Features</a>
+        </li>
+        <li class="nav-item">
+          <a href="BlogPublic.php?page=1" class="nav-link">View Posts</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item"><a href="Logout.php" class="nav-link text-danger">
+          <i class="fas fa-user-times"></i> Login</a></li>
+      </ul>
+      </div>
+    </div>
+  </nav>
+    <div style="height:10px; background:#27aae1;"></div>
+    <!-- NAVBAR END -->
+    <!-- HEADER -->
+    <header class="bg-dark text-white py-3">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+          <h1><i class="fas fa-text-height" style="color:#27aae1;"></i> SoftNet IT Infrastructure</h1>
+          </div>
+        </div>
+      </div>
+    </header>
+    <!-- HEADER END -->
+    <section id="newsletter">
+      <div class="container">
+              <h1>Subscribe to our newsletter</h1>
+              <form action="#">
+                  <input type="email" placeholder="Enter e-mail..." required>
+                  <button type="submit" class="button">Subscribe</button>
+              </form>
+      </div>
+  </section>
+  <div class="container" style="margin-top: 1rem;">
+    <div id="back_to_top"></div>
+    <ul type="none">
+      <a href="#Objectives" style="text-decoration: none;"><li><i class="fa fa-arrow-circle-down"></i> Objectives</li></a>
+      <a href="#Mission" style="text-decoration: none;"><li><i class="fa fa-arrow-circle-down"></i> Mission</li></a>
+      <a href="#Vision" style="text-decoration: none;"><li><i class="fa fa-arrow-circle-down"></i> Visions</li></a>
+      <a href="#Core_Values" style="text-decoration: none;"><li><i class="fa fa-arrow-circle-down"></i> Core Values</li></a>
+    </ul>
+  </div>
+  <hr>
+  <div class="container" style="margin-top: 1rem;">
+    <p><strong>SOFTNET,</strong> a non-for Profit Organization, was conceptualized and established in 
+      2019 by innovative and dedicated young Liberian I.T Professionals who believe that the computer 
+      industry plays a major role in positive shifting the future of our beloved country 
+      mama-Liberia through providing computer education to everyone in all sectors and aspect 
+      of life regardless of financial status, ethnic background, religious affiliation or political views. 
+      SOFTNET remains dedicated to providing an array of professional-hands-on computer knowledge and solutions to all.
+    </p>
+  </div>
+    <hr>
+  <div class="container">
+    <h1 id="Objectives">Objectives</h1>
+    <p>SOFTNET LIBERIA was established to help train potential computer users in Liberia to 
+      enhance the smooth operation in their various areas of work in all sectors such as in 
+      Health, Education, and Security; to discover their truth career path in the computer 
+      industry, through giving Liberian the foundational skills necessary for them to succeed
+      in their work places, and increase their knowledge with computers, Networking and also
+      to connect them to the right career path in the Computer Industry. Below are some 
+      core objectives:  
+      <ul type="none" style="margin:2px;">
+        <li style="margin: 6px;"><i class="fas fa-desktop"></i> create a conducive Computer learning environment for all Liberian </li>
+        <li style="margin: 6px;"><i class="fas fa-user-graduate"></i> To make computer learning affordable for all Liberian </li>
+        <li style="margin: 6px;"><i class="fas fa-book-reader"></i> To create the mindset in each individual to model a real-world 
+          problem in Liberia into a computer based application for better 
+          solution that will improve our Liberian society 
+        </li>
+        <li style="margin: 4px;"><i class="fas fa-microscope"></i> To create a suitable and reliable research environment for all.</li>
+      </ul>
+    </p>
+    <a href="#back_to_top" style="text-decoration: none;">Back to top <i class="fa fa-arrow-circle-up"></i></a>
+  </div>
+  <hr>
+  <div class="container">
+    <h1 id="Mission">Mission</h1>
+      <p>
+        SofNet-Liberia, a non-for Profit Organization established in 2019, is dedicated to providing computer education 
+       and/or solutions to everyone in all sectors and aspect of life regardless of social or economic status, religious affiliation, 
+        political views or ethnic background. SoftNet Liberia Designs and promote the platform where all Liberian who desires 
+        to start a career in the field of Information Technology (I.T) or Information Community & Technology (ICT) can have an 
+        affordable access to learn computer or advanced on an existing computer knowledge and be able to solve real-world problems 
+        effecting their communities or society. 
+      </p>
+      <a href="#back_to_top" style="text-decoration: none;">Back to top <i class="fa fa-arrow-circle-up"></i></a>
+  </div>
+  <hr>
+  <div class="container">
+    <h1 id="Vision">Vision</h1>
+    <p>
+      <h4>SoftNet Liberia envisioned to:</h4>
+      <ul type="none" style="margin:2px;">
+        <li style="margin: 6px;"><i class="fas fa-desktop"></i> See every Liberian become computer literate (to increase computer literacy rate in Liberia)</li>
+        <li style="margin: 6px;"><i class="fas fa-user-graduate"></i> See students become competent researchers using computer base research methodology</li>
+        <li style="margin: 6px;"><i class="fas fa-book-reader"></i> See those who are computer literate become employ in various institutions of work 
+          that need competent I.T. employees 
+        </li>
+        <li style="margin: 4px;"><i class="fas fa-microscope"></i> To see that 45 – 50% of various institutions’ operation be performed by computer users (computerize operations)</li>
+      </ul>
+    </p>
+    <a href="#back_to_top" style="text-decoration: none;">Back to top <i class="fa fa-arrow-circle-up"></i></a>
+  </div>
+  <hr>
+  <div class="container">
+    <h1 id="Core_Values" name="corevalues">Core Values</h1>
+    <p>
+      The core values of SoftNet Liberia are very critical to the success of 
+      the organization. They are the strong foundation of our operations, 
+      they define who we are, and set us apart from any other organization. 
+      They are motivating factors to our vision. These values include but 
+      are not limited to: 
+      <ul type="none" style="margin:2px;">
+        <li style="margin: 6px;"><i class="fas fa-desktop"></i> Integrity </li>
+        <li style="margin: 6px;"><i class="fas fa-user-graduate"></i> Timeliness</li>
+        <li style="margin: 6px;"><i class="fas fa-book-reader"></i> Responsiveness </li>
+        <li style="margin: 4px;"><i class="fas fa-microscope"></i> Effectiveness</li>
+        <li style="margin: 6px;"><i class="fas fa-user-graduate"></i> Transparency</li>
+        <li style="margin: 6px;"><i class="fas fa-book-reader"></i> Accountability</li>
+        <li style="margin: 4px;"><i class="fas fa-microscope"></i>  Quality</li>
+        <li style="margin: 4px;"><i class="fas fa-microscope"></i> Innovation</li>
+      </ul>
+    </p>
+    <a href="#back_to_top" style="text-decoration: none;">Back to top <i class="fa fa-arrow-circle-up"></i></a>
+  </div>
+  <hr id="hr_tag">
+  <section id="boxes">
+    <div class="container">
+      <h1 style="text-align: center;">Our Engagements</h1>
+      <div class="box">
+          <img src="Images/img/Career.jpg" alt="Image not available">
+          <h3>Leadership</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto eligendi accusantium optio aliquam esse beatae porro, accusamus aspernatur soluta sapiente alias, dolorem hic, excepturi maxime!</p>
+          <span><button type="button">Read More</button></span>
+        </div>
+      <div class="box">
+          <img src="Images/img/Lead.jpg" alt="">
+          <h3>Career Development</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto eligendi accusantium optio aliquam esse beatae porro, accusamus aspernatur soluta sapiente alias, dolorem hic, excepturi maxime!</p>
+          <span><button type="button">Read More</button></span>
+        </div>
+      <div class="box">
+          <img src="Images/img/Volunteer.jpeg" alt="Image not available">
+          <h3>Volunteerism</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto eligendi accusantium optio aliquam esse beatae porro, accusamus aspernatur soluta sapiente alias, dolorem hic, excepturi maxime!</p>
+          <span><button type="button">Read More</button></span>
+        </div>
+    </div>
+  </section>
+  <div class="container">
+    <a href="#back_to_top" style="text-decoration: none;">Back to top <i class="fa fa-arrow-circle-up"></i></a>
+  </div>
+<br>
+    <!-- FOOTER -->
+    <footer class="bg-dark text-white">
+      <div class="container">
+        <div class="row">
+          <div class="col">
+          <p class="lead text-center">Theme By | SoftNet.com | <span id="year"></span> &copy; ----All right Reserved.</p>
+          <p class="text-center small"><a style="color: white; text-decoration: none; cursor: pointer;" href="http://facebook.com" target="_blank">SoftNet.com have all the rights, no one is allow to distribute copies other then <br>SoftNet.com&trade;,  SoftNet IT Infrastruture & Skills&trade;</a></p>
+           </div>
+         </div>
+      </div>
+    </footer>
+        <div style="height:10px; background:#27aae1;"></div>
+    <!-- FOOTER END-->
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+<script>
+  $('#year').text(new Date().getFullYear());
+</script>
+</body>
+</html>
---
+++ Includes/DB.php
@@ -0,0 +1,6 @@
+<?php
+$DSN='mysql:host = localhost; dbname=cms4.2.1';
+$ConnectingDB = new PDO($DSN,'root','root');
+
+
+
